c7c12d12b8653f921755c8cda4d1a356180e8204
Olga Zinoveva <ozinka81@gmail.com>
2019-11-07 07:20:33 +0100

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
+export default Header;
+
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '../Context/AuthContext';
 
 class Header extends Component {
   render() {
+    const { isLoggedin, handleLogout } = this.props;
+
     return (
-      <div className="header">
-        <a href="/">
-          <img src="/images/home.jpg" alt="home"></img>
-        </a>
+      <div className='app-header'>
+        <div className="app-title"><Link to="/"> Boozemeter </Link></div>
+        {isLoggedin ? (
+          <button className='logout-btn' onClick={handleLogout}>Log out</button>
+        ) : (
+          <Link className='login-link' to="/login">Login</Link>
+        )}
+
       </div>
     );
   }
 }
 
-export default Header;
+export default withAuth(Header);
