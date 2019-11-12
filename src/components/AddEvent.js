ea22368dd6d6a0221c6e792658700e6a308251ed
Olga Zinoveva <ozinka81@gmail.com>
2019-11-12 17:29:01 +0100

    addEvent view component and style

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -5,6 +5,7 @@ import eventService from '../services/eventService';
 class AddEvent extends Component {
   state = {
     result: '',
+    date: null,
     name: '',
     type: '',
     percentage: '',
@@ -26,48 +27,84 @@ class AddEvent extends Component {
     await eventService.getHealthLabels().then(data =>
       this.setState({
         healthLabels: data,
-        type: 'All is fine',
+        health: 'All is fine',
       }),
     );
+  }
 
-    handleChange = event => {
-      const { name, value } = event.target;
-      this.setState({
-        [name]: value,
-      });
-    };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
- handleSubmit = event => {
+  handleSubmit = event => {
+    event.preventDefault();
     const { name, type, percentage, cost, volume, health } = this.state;
     const newEvent = { name, type, percentage, cost, volume, health };
     eventService
       .addEvent(newEvent)
       .then(data =>
         this.setState({
-            result: "Drink added",
+          result: 'Drink added',
         }),
-       ) 
-       .catch(err =>
+      )
+      .catch(err =>
         this.setState({
           result: err.response.data.code,
         }),
       );
- };
-  
- 
- render(){
+  };
+
+  render() {
     const { result, name, type, percentage, cost, volume, health, drinkLabels, healthLabels } = this.state;
-    
-
-
-
-
-
-
- }
-
-
- 
+    return (
+      <div className="add-event-form">
+        <form noValidate onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <label className="name-label">Name:</label>
+            <input type="text" name="name" value={name} onChange={this.handleChange} />
+          </div>
+          <div className="input-group">
+            <label className="type-label">Type:</label>
+            <select name="type" value={type} onChange={this.handleChange}>
+              {drinkLabels.map(item => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="input-group">
+            <label className="percentage-label">Percentage:</label>
+            <input type="text" name="percentage" value={percentage} onChange={this.handleChange} />
+          </div>
+          <div className="input-group">
+            <label className="cost-label">Cost(in cents):</label>
+            <input type="text" name="cost" value={cost} onChange={this.handleChange} />
+          </div>
+          <div className="input-group">
+            <label className="volume-label">Volume:</label>
+            <input type="text" name="volume" value={volume} onChange={this.handleChange} />
+          </div>
+          <div className="input-group">
+            <label className="health-label">Type:</label>
+            <select name="health" value={health} onChange={this.handleChange}>
+              {healthLabels.map(item => (
+                <option value={item} key={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button type="submit" className="event-submit-btn">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default withRouter(AddEvent);;
+export default withRouter(AddEvent);
--- src/views/AddEventView.js
+++ src/views/AddEventView.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import AddEvent from '../components/AddEvent';
 
 class AddEventView extends Component {
   // eslint-disable-next-line class-methods-use-this
   render() {
-    return <div>Hello! aqui va AddEventView</div>;
+    return (
+      <div className="add-event-screen">
+        <div className="view-heading">Add new drink</div>
+        <AddEvent />
+      </div>
+    );
   }
 }
 
